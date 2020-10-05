1f6d13bbcac292573d38d9c200356a39f3f662b6
Bang71 <es.bang71@gmail.com>
2020-10-05 10:54:18 +0900

    Simplify code

--- Q12954/Solution.kt
+++ Q12954/Solution.kt
@@ -1,13 +1,3 @@
 class Solution {
-    fun solution(x: Int, n: Int): LongArray {
-        var answer = longArrayOf()
-        var total: Long = 0
-        
-        repeat(n) {
-            total += x.toLong()
-            answer += total
-        }
-        
-        return answer
-    }
+    fun solution(x: Int, n: Int): LongArray = LongArray(n) { x.toLong() * (it + 1) }
 }
